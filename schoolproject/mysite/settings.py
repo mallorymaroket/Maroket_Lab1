15f99a07a569efcb779471983989b1deba21d188
mallorymaroket <mallory.maroket@obf.ateneo.edu>
2021-03-18 23:09:39 +0800

    Unfinished Profile Page
    All the content needed for the profile.html is there, except for the image. Still figuring out how to work with static files.

--- schoolproject/mysite/settings.py
+++ schoolproject/mysite/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static")
+]
