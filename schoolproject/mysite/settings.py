8f8d770d03a30f5b17cad1586ecc7aaa8586e77c
mallorymaroket <mallory.maroket@obf.ateneo.edu>
2021-03-19 03:15:12 +0800

    Fixed image
    Successfully displayed image in profile.html.

--- schoolproject/mysite/settings.py
+++ schoolproject/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/image/'
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
